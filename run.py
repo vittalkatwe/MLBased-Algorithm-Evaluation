8b907e663e8a3207038de6baf021dc1c5e49c8e7
Vittal Katwe <109062125+vittalkatwe@users.noreply.github.com>
2025-02-07 17:08:49 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,12 +1,15 @@
 
+
 from waitress import serve
 from app import app
 import logging
+import os  # Import os module
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
     logger.info("Starting the Waitress server...")
-    serve(app, host='0.0.0.0', port=8000, threads=10)  # Explicitly set threads for concurrency
+    port = int(os.environ.get("PORT", 8000))
+    serve(app, host='0.0.0.0', port=port, threads=10)
     logger.info("Server stopped.")
